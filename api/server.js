73dca2ab11e995bded1c9b7ab948dddf4bc721fa
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-18 12:37:15 -0400

    feature/auth routers and middleware implemented

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const express = require( 'express' );
 const helmet = require( 'helmet' );
 const cors = require( 'cors' );
 
-const plantsRouter = require('../plants/plants-router.js')
-const usersRouter = require('../users/users-router');
+const authRouter = require( '../auth/auth-router.js' );
+const plantsRouter = require( '../plants/plants-router.js' )
+const restricted = require( '../auth/restricted-middleware.js' );
+const usersRouter = require( '../users/users-router' );
 
 const server = express();
 
@@ -11,8 +13,9 @@ server.use( helmet() );
 server.use( express.json() );
 server.use( cors() );
 
-server.use('/api/plants', plantsRouter);
-server.use('/api/users', usersRouter);
+server.use( '/api/auth', authRouter );
+server.use( '/api/plants', restricted, plantsRouter );
+server.use( '/api/users', usersRouter );
 
 server.get( '/', (req, res) => {
     res.json({ api: 'up' });
---
+++ auth/restricted-middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken'); 
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+    const secret = process.env.JWT_SECRET || 'is it secret, is it safe?';
+
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                // something wrong with the token
+                res.status(401).json({ you: 'can\'t do that!' });
+            } else {
+                // token is good we can see the data inside the decodedToken
+                req.jwt = decodedToken;
+
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({ you: 'shall not pass!' });
+    }
+};
