4ab4ae0e2d1257cccdc1d686645374ee5be46ca0
Brian Kubes <briankubes@gmail.com>
2021-04-14 19:16:16 -0700

    changed dbConfig.js to have 'const db = knex(config[environment])' instead of 'knex(config)'

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,7 @@
-const knex = require( 'knex') ;
+const knex = require("knex");
+const config = require("../knexfile.js");
+const environment = process.env.DB_ENV || "development";
 
-const environment = process.env.DB_ENV || 'development';
-const config = require( '../knexfile.js' )[environment];
+const db = knex(config[environment]);
 
-module.exports = knex(config);
+module.exports = db;
